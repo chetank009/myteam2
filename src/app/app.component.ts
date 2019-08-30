11ef1acd3e1a3571895cf5083b073e4f18b71591
User1 <user1@Skycliffitdomain.com>
2019-08-30 14:40:07 +0530

    team My Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+import {MatDialog} from '@angular/material/dialog';
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  xpandStatus=true;
   title = 'team2';
 }
+export class InputFormExample {}
+
+export class CheckboxOverviewExample {}
+
+export class DialogContentExample {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    // tslint:disable-next-line: no-use-before-declare
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
+export class DialogContentExampleDialog {}
+
+
+
+// onclick()
+// {
+// <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-xl">Extra large modal</button>
+
+// <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+//   <div class="modal-dialog modal-xl">
+//     <div class="modal-content">
+//       ...
+//     </div>
+//   </div>
+// </div>
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule, MatTabsModule } from '@angular/material';
+
+import {MatInputModule} from '@angular/material/input';
+
+import {MatSelectModule} from '@angular/material/select';
+
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+import {MatDialogModule} from '@angular/material/dialog';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatDialogModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
